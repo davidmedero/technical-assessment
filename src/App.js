9e7574215c35e0363646d7e7c722f1cdca433809
davidmedero <david.gregory.medero@gmail.com>
2021-06-07 01:09:10 -0400

    finished react task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import React from "react";
 
-function App() {
+const App = () => {
+  const [books, setBooks] = React.useState([
+    {
+      id: 1,
+      name: "First book",
+    },
+  ]);
+  const [currentBookName, setCurrentBookName] = React.useState("");
+
+  function renderItems() {
+    return books.map((book) => {
+      return (
+        <li className="list-group-book book row" key={book.id}>
+          <span className="book-name">{book.name}</span>
+          <button
+            className="delete-book btn btn-danger"
+            onClick={() => deleteBook(book)}
+          >
+            Delete
+          </button>
+        </li>
+      );
+    });
+  }
+
+  function addBook() {
+    let copy = [...books, { id: books.length + 1, name: currentBookName }];
+    setBooks(copy);
+  }
+
+  function deleteBook(book) {
+    let copy = [...books];
+    let remove = copy.filter((el) => el.id !== book.id);
+    setBooks(remove);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div className="form-row add-book-form container">
+        <input
+          id="newBookName"
+          placeholder="Enter book name"
+          type="text"
+          className="form-control"
+          value={currentBookName}
+          onChange={(e) => setCurrentBookName(e.target.value)}
+        />
+        <button
+          id="addBook"
+          className="btn btn-primary"
+          onClick={() => addBook()}
         >
-          Learn React
-        </a>
-      </header>
+          Add Book
+        </button>
+      </div>
+      <hr />
+      <ul className="book-list container">{renderItems()}</ul>
     </div>
   );
-}
+};
 
 export default App;
